770fff3012d947250d41200b7d582d4369621c5a
Max <max.hoheiser@gmail.com>
2020-07-20 21:26:52 +0200

    changes_max

---
+++ nma_class.py
@@ -0,0 +1,14 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import csv
+import scipy.stats as st
+import platform
+import os
+
+"""
+Class for all the functions from Samual, Kcenia and Max NMA Projekt
+
+- put in finalized functions - production ready
+- pay close attantion on documentation and help files
+"""
